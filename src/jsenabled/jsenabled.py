ab746ba2f3819e6e35f0976a7b6b54fadb9a7566
woodsprite1984 <woodsprite1984@gmail.com>
2009-05-11 17:44:25 +0000

    validator

--- src/jsenabled/jsenabled.py
+++ src/jsenabled/jsenabled.py
@@ -25,7 +25,17 @@ class AdminPage(GeneralPage):
         self.response.out.write(pocket.render_template(config.TEMPLATE_PATH+'admin.html'))
     def post(self):
         if self.request.get('actiontype') == '1':
-            service.add_to(service.Tag, name=self.request.get('name'), description=self.request.get('description'), owner=[self.request.get('owner')], type=int(self.request.get('type')))
+            _data_dict={
+                'name': self.request.get('name'),
+                'description': self.request.get('description'),
+                'owner': [self.request.get('owner')],
+                'type': int(self.request.get('type')),
+                }
+            if pocket.Validator('validator_Tag').validate(**_data_dict):
+                service.add_to(service.Tag, **_data_dict)
+                self.response.out.write('Addition successfully')
+            else:
+                self.response.out.write('Validate failed!')
         elif self.request.get('actiontype') == '2':
             service.delete_from(service.Tag, name=self.request.get('name'))
         elif self.request.get('actiontype') == '3':
@@ -36,10 +46,6 @@ class MainPage(GeneralPage):
         _tags = service.select_from(service.Tag, all=True)
         for _tag in _tags:
             self.response.out.write(_tag.to_xml())
-        self.response.out.write(str(service.is_exist(service.Tag, name='python')))
-        self.response.out.write(str(pocket.in_period('wang', 2, 5, len)))
-        self.response.out.write(str(bool(pocket.search_pattern('162.105.124',r'(\d+\.){3}(\d+)'))))
-        self.response.out.write(str(bool(pocket.can_be_int('-1'))))
     def post(self):
         pass
 
