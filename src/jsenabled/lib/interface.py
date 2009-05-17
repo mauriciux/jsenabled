b510d8226a9ab3630d8b0f01abf748995e35641a
woodsprite1984 <woodsprite1984@gmail.com>
2009-05-17 15:25:39 +0000

    add_article half

--- src/jsenabled/lib/interface.py
+++ src/jsenabled/lib/interface.py
@@ -86,6 +86,15 @@ def modify_user(from_key=None, from_user_id=None, **attr_dict):
         return (service.modify_on(service.User, _from_dict, _to_dict) if _validator.validate(**_to_dict) else 0)
     return 0 
 
+
+# BaseArticle
+def select_basearticle(key=None, id_num=None):
+    if key:
+        return service.BaseArticle.get_by_key_name(key)
+    if id_num:
+        return service.select_from(service.BaseArticle, id_num=id_num)
+    return None
+
 # Comment
 def add_comment(ancestor_key, **attr_dict):
     _validator = pocket.Validator('vBaseArticle')
@@ -94,17 +103,18 @@ def add_comment(ancestor_key, **attr_dict):
         if _validator.validate(ancestor_key=ancestor_key, **attr_dict):
             _self_key = service.add_to(service.Comment, ancestor_key=ancestor_key, **attr_dict)
             if _self_key:
-                _entry = select_basearticle(basearticle_key=_self_key)
+                _entry = select_basearticle(key=_self_key)
                 _entry.comment_list.append(_self_key)
                 _entry.put()
                 return True
     return False
 
-# BaseArticle
-def select_basearticle(basearticle_key=None, basearticle_id_num=None):
-    if basearticle_key:
-        return service.BaseArticle.get_by_key_name(basearticle_key)
-    if basearticle_id_num:
-        return service.select_from(service.BaseArticle, id_num=basearticle_id_num)
-    return None
-
+# LatestArticle
+def add_latestarticle(**attr_dict):
+    _validator1 = pocket.Validator('vBaseArticle')
+    _validator2 = pocket.Validator('vFormalArticle')
+    _validator3 = pocket.Validator('vLatestArticle')
+    if (_validator1.validate(**attr_dict) and _validator2.validate(**attr_dict) and _validator3.validate(**attr_dict)):
+        service.add_to(service.LatestArticle, **attr_dict)
+        return True
+    return False
